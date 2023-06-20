89b5ccccbe85888543e42135a94a2e732d109273
Giovanna Rodrigues <giovannasimionero@gmail>
2023-06-20 13:49:20 -0300

    primeiro

---
+++ DateTimePicker_Certo/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace DateTimePicker_Certo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dtp_data = new System.Windows.Forms.DateTimePicker();
+            this.btn_obterData = new System.Windows.Forms.Button();
+            this.btn_setarData = new System.Windows.Forms.Button();
+            this.tb_data = new System.Windows.Forms.TextBox();
+            this.tb_dia = new System.Windows.Forms.TextBox();
+            this.tb_ano = new System.Windows.Forms.TextBox();
+            this.tb_mes = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Verdana", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(81, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(241, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Agendamento";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.pictureBox1.Location = new System.Drawing.Point(87, 82);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(10, 22);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Verdana", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(103, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(268, 14);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Agende seu horario para uma consulta";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(84, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 14);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Nome:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(84, 189);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 14);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Telefone:";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(159, 187);
+            this.maskedTextBox1.Mask = "(00) 00000-0000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox1.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(132, 153);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(127, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(84, 269);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(127, 14);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Escolha uma data:";
+            // 
+            // dtp_data
+            // 
+            this.dtp_data.CustomFormat = "d";
+            this.dtp_data.Location = new System.Drawing.Point(87, 319);
+            this.dtp_data.Name = "dtp_data";
+            this.dtp_data.Size = new System.Drawing.Size(235, 20);
+            this.dtp_data.TabIndex = 8;
+            this.dtp_data.ValueChanged += new System.EventHandler(this.dtp_data_ValueChanged);
+            // 
+            // btn_obterData
+            // 
+            this.btn_obterData.Location = new System.Drawing.Point(87, 345);
+            this.btn_obterData.Name = "btn_obterData";
+            this.btn_obterData.Size = new System.Drawing.Size(116, 23);
+            this.btn_obterData.TabIndex = 9;
+            this.btn_obterData.Text = "obter data";
+            this.btn_obterData.UseVisualStyleBackColor = true;
+            this.btn_obterData.Click += new System.EventHandler(this.btn_obterData_Click);
+            // 
+            // btn_setarData
+            // 
+            this.btn_setarData.Location = new System.Drawing.Point(209, 345);
+            this.btn_setarData.Name = "btn_setarData";
+            this.btn_setarData.Size = new System.Drawing.Size(113, 23);
+            this.btn_setarData.TabIndex = 10;
+            this.btn_setarData.Text = "setar data";
+            this.btn_setarData.UseVisualStyleBackColor = true;
+            this.btn_setarData.Click += new System.EventHandler(this.btn_setarData_Click);
+            // 
+            // tb_data
+            // 
+            this.tb_data.Location = new System.Drawing.Point(87, 403);
+            this.tb_data.Name = "tb_data";
+            this.tb_data.Size = new System.Drawing.Size(235, 20);
+            this.tb_data.TabIndex = 11;
+            // 
+            // tb_dia
+            // 
+            this.tb_dia.Location = new System.Drawing.Point(87, 434);
+            this.tb_dia.Name = "tb_dia";
+            this.tb_dia.Size = new System.Drawing.Size(75, 20);
+            this.tb_dia.TabIndex = 12;
+            this.tb_dia.TextChanged += new System.EventHandler(this.tb_dia_TextChanged);
+            // 
+            // tb_ano
+            // 
+            this.tb_ano.Location = new System.Drawing.Point(247, 434);
+            this.tb_ano.Name = "tb_ano";
+            this.tb_ano.Size = new System.Drawing.Size(75, 20);
+            this.tb_ano.TabIndex = 13;
+            // 
+            // tb_mes
+            // 
+            this.tb_mes.Location = new System.Drawing.Point(166, 434);
+            this.tb_mes.Name = "tb_mes";
+            this.tb_mes.Size = new System.Drawing.Size(75, 20);
+            this.tb_mes.TabIndex = 14;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(166, 374);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "hoje";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::DateTimePicker_Certo.Properties.Resources._1ab60c31_1811_410b_ac74_69ad2b92ddef;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(855, 492);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.tb_mes);
+            this.Controls.Add(this.tb_ano);
+            this.Controls.Add(this.tb_dia);
+            this.Controls.Add(this.tb_data);
+            this.Controls.Add(this.btn_setarData);
+            this.Controls.Add(this.btn_obterData);
+            this.Controls.Add(this.dtp_data);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "DateTimePicker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dtp_data;
+        private System.Windows.Forms.Button btn_obterData;
+        private System.Windows.Forms.Button btn_setarData;
+        private System.Windows.Forms.TextBox tb_data;
+        private System.Windows.Forms.TextBox tb_dia;
+        private System.Windows.Forms.TextBox tb_ano;
+        private System.Windows.Forms.TextBox tb_mes;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ DateTimePicker_Certo/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DateTimePicker_Certo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_obterData_Click(object sender, EventArgs e) // cassiane
+        {
+            //para buscar a data que esta no datapicker
+            tb_data.Text = dtp_data.Text;
+            tb_dia.Text = dtp_data.Value.Day.ToString();//retorna um inteiro
+            tb_mes.Text = dtp_data.Value.Month.ToString();
+            tb_ano.Text = dtp_data.Value.Year.ToString();
+        }
+
+        private void btn_setarData_Click(object sender, EventArgs e) // giovanna
+        {
+            int d, m, a; // facilitar
+            d = Int32.Parse(tb_dia.Text); //int parse funciona como uma conversão de string para int
+            m = Int32.Parse(tb_mes.Text);
+            a = Int32.Parse(tb_ano.Text);
+
+            DateTime dt = new DateTime(a, m, d); //inicializa uma nova estância de estrutura para o ano, mês e dia especificado, resetando os comandos acima.
+
+            dtp_data.Value = dt; // Value define o valor de data/hora atribuido.
+        }
+
+        private void dtp_data_ValueChanged(object sender, EventArgs e)// cassiane
+        {
+            this.dtp_data.MinDate = DateTime.Today;
+        }
+
+        private void button3_Click(object sender, EventArgs e) // giovanna
+        {
+            dtp_data.Value = DateTime.Today; // Value define o valor de data/hora atribuido. = a data atual dde hoje
+        }
+
+        private void tb_dia_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
